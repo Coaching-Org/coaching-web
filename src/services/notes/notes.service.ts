adf5b0ea8b5e622d2c658f0fd0b3af6e00de0811
Tatas Fachrul <tatasfachrul@gmail.com>
2024-11-27 18:28:47 +0700

    Change api

--- src/services/notes/notes.service.ts
+++ src/services/notes/notes.service.ts
@@ -1,8 +1,13 @@
 import { PostNotesRequest } from "@/interfaces/notes/post-notes.type";
 import { HttpService } from "../HttpServices";
+import { GetNotesRequest } from "@/interfaces/notes/get-notes.type";
 
 export class NotesServices extends HttpService {
   static postNotes(params: PostNotesRequest) {
     return this.post("/v1/notes", params);
+  }
+
+  static getNotesList(params: GetNotesRequest, signal?: AbortSignal) {
+    return this.get("/v1/notes", { params, signal });
   }
 }
