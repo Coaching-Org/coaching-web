6d10cc2ff6f91d1a6f9cf8e44e3cecb4fe40bb3c
Tatas Fachrul <tatasfachrul@gmail.com>
2024-12-03 18:10:49 +0700

    Change response to csv file

--- src/hooks/functions/Export.utils.ts
+++ src/hooks/functions/Export.utils.ts
@@ -5,7 +5,7 @@ import {
   useExportAllCoachesQuery,
   useExportCoachesAppointmentsQuery,
 } from "../query/export/exports.query";
-
+import moment from "moment";
 export const useExportUtils = () => {
   const [coachId, setCoachId] = useState<number>(0);
   const { data: exportAppointments, refetch: refetchExportAppointments } =
@@ -41,6 +41,42 @@ export const useExportUtils = () => {
     }
   }, [coachId]);
 
+  useEffect(() => {
+    if (exportAppointments) {
+      downloadCSV(
+        exportAppointments,
+        `${moment().format("YYYY-MM-DD")}_export_appointments.csv`
+      );
+    }
+  }, [exportAppointments]);
+
+  useEffect(() => {
+    if (exportNotes) {
+      downloadCSV(
+        exportNotes,
+        `${moment().format("YYYY-MM-DD")}_export_notes.csv`
+      );
+    }
+  }, [exportNotes]);
+
+  useEffect(() => {
+    if (exportAllCoaches) {
+      downloadCSV(
+        exportAllCoaches,
+        `${moment().format("YYYY-MM-DD")}_export_all_coaches.csv`
+      );
+    }
+  }, [exportAllCoaches]);
+
+  useEffect(() => {
+    if (exportCoachesAppointments) {
+      downloadCSV(
+        exportCoachesAppointments,
+        `${moment().format("YYYY-MM-DD")}_export_coaches_appointments.csv`
+      );
+    }
+  }, [exportCoachesAppointments]);
+
   return {
     state: {
       exportAppointments,
@@ -55,4 +91,18 @@ export const useExportUtils = () => {
       getExportCoachesAppointments,
     },
   };
+};
+
+const downloadCSV = (data: string, filename: string) => {
+  const blob = new Blob([data], { type: "text/csv;charset=utf-8;" });
+  const link = document.createElement("a");
+  const url = URL.createObjectURL(blob);
+
+  link.setAttribute("href", url);
+  link.setAttribute("download", filename);
+  link.style.visibility = "hidden";
+
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 };
