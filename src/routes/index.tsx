520bb583b88fd2eb90da701f570e9a271cdd4657
Tatas Fachrul <tatasfachrul@gmail.com>
2024-12-09 16:26:06 +0700

    Change text size and default login routing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,8 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
+import { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
 import HyperText from "@/components/ui/hyper-text";
 import ShinyButton from "@/components/ui/shiny-button";
 import { useLanguage } from "@/components/language.provider";
+import { useEffect } from "react";
 
 export const Route = createFileRoute("/")({
   component: Home,
@@ -9,6 +10,10 @@ export const Route = createFileRoute("/")({
 
 function Home() {
   const { translations } = useLanguage();
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate({ to: "/login" });
+  }, []);
   return (
     <div className="grid min-h-screen w-full place-items-center md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="flex flex-col items-center gap-9 mb-4 col-span-full">
